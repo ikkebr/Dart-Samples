4036ad87cba8f00562ca186a19c87613cfae02f3
ikkebr <ikkibr@gmail.com>
2011-10-10 13:15:54 -0300

    Now Fast Fib works. Added some insterions to check for errors.

--- fib.dart
+++ fib.dart
@@ -8,7 +8,7 @@ int fib(n){
   if (n % 2 == 0){
     fibs[n] = ( (2 * fib((n / 2.0) - 1)) + fib(n / 2.0)) * fib(n / 2.0);
   } else {
-    fibs[n] = Math.pow(fib((n - 1) / 2.0), ^ 2) + Math.pow(fib((n + 1) / 2.0), 2);
+    fibs[n] = Math.pow(fib((n - 1) / 2.0), 2) + Math.pow(fib((n + 1) / 2.0), 2);
   }
 
   return fibs[n];
@@ -17,6 +17,12 @@ int fib(n){
 main() {
   fibs[0] = 0;
   fibs[1] = 1;
+  
+  assert(fibs[0] == 0);
+  assert(fibs[1] == 1);
+  assert(fib(2) == (fibs[0]+fibs[1]));
+  assert(fib(3) == (fibs[2]+fibs[1]));
+  assert(fib(10) == fib(9)+fib(8));
   
   for (var i=0; i<10; i++){
 	print(fib(i));
