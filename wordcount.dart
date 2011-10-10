000e3836a1c324ac6175e0c3b239169dd1985995
ikkebr <ikkibr@gmail.com>
2011-10-10 14:54:07 -0300

    Added sorting to the word counter.

--- wordcount.dart
+++ wordcount.dart
@@ -9,6 +9,18 @@ int howmany(Collection collection, element){
 	return occurrences;
 }
 
+int dictsort(e1, e2){
+  if (e1.getKeys()[0] > e2.getKeys()[0]){
+    return 1;
+  } else if (e1.getKeys()[0] < e2.getKeys()[0]) {
+    return -1;
+  } else {
+    return e1.getValues()[0].compareTo(e2.getValues()[0]);
+  }
+
+  return 0;
+}
+
 main() {
   String text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in nisl eu neque pharetra vulputate. Ut congue risus at nulla vestibulum nec tristique metus semper. Aliquam aliquam ipsum commodo nisi sagittis facilisis fringilla sem ornare. Cras ac nisi massa, eget malesuada dui. Curabitur in tristique eros. Aliquam et nulla ligula, ac mattis lacus. Nulla purus nibh, consequat et dignissim et, commodo id felis. Donec sit amet orci non lectus auctor commodo. Donec sem ligula, scelerisque sagittis faucibus sit amet, feugiat viverra ipsum. Nullam non mi mi. Nunc consequat ultrices varius. Pellentesque eros nisi, suscipit vitae laoreet a, vestibulum sed lectus. Nam ut semper lacus. Sed scelerisque velit aliquam nisl tempus non pharetra neque malesuada. Praesent semper leo et justo dictum nec vehicula dui mollis.';
   text = text.replaceAll(',',' ');
@@ -31,8 +43,18 @@ main() {
 	counter[word] = howmany(words, word);
   }
   
+  var ordered_list = new List<Map<int,String>>();
+
   counter.forEach( (word, number) {
-    print("${word}\t${number}");
+    var temp = new Map<int,String>();
+    temp[number] = word;
+    ordered_list.add(temp);
   });
+
+  ordered_list.sort(dictsort);
+
+  for(var i=0; i<ordered_list.length; i++){
+    print("${ordered_list[i].getKeys()[0]} ${ordered_list[i].getValues()[0]}");
+  }
 
 }
